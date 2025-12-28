1096af440379d10445560c1f43b40bbea5de46d4
AnPavlov7070 <redzek15@gmail.com>
2025-12-28 16:05:54 +0200

    Testing Gitleaks

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 const { createServer } = require('node:http');
 
 // ❌ INTENTIONAL HARDCODED SECRET (for Gitleaks testing only)
-const DB_PASSWORD = "SuperSecretPassword123!";
+const DB_PASSWORD = "SuperSecretPassword1234!";
 
 const hostname = '0.0.0.0';
 const port = Number(process.env.PORT) || 8000;
